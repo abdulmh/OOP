a1b4f690c6ce277983a89ad7141ef65e699b107a
abdulmh <abdul-malikh@chc.edu>
2022-09-01 11:02:45 -0400

    OOP Project

---
+++ src/Major.java
@@ -0,0 +1,8 @@
+// Polymorhism is to override methods to change their behavior
+public class Major {
+    
+    // Define two methods
+    public Integer creditsRemaining(Integer creditsRequired);
+    public Boolean isMajorComplete(Integer creditsRequired);
+
+}
---
+++ src/undergradStudent.java
@@ -0,0 +1,59 @@
+// Inheretance is to use class hierarchies to enabel the reuse of functionality 
+public class undergradStudent extends Student implements Major{
+
+    private Double gpa;
+    private Integer creditsEarned;
+    private String major;
+
+    public undergradStudent(String firstName, String lastName, String schoolName, Double gpa, Integer creditsEarned, String major) {
+            super(firstName, lastName, schoolName);
+
+        this.gpa = gpa;
+        this.creditsEarned = creditsEarned;
+        this.major = major;
+    }
+    
+    public Double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(Double gpa) {
+        this.gpa = gpa;
+    }
+
+    public Integer getCreditsEarned() {
+        return creditsEarned;
+    }
+
+    public void setCreditsEarned(Integer creditsEarned) {
+        this.creditsEarned = creditsEarned;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    // Polymorphism is to override methods to change their behavior
+    @Override
+    public Integer creditsRemaining(Integer creditsRequired) {
+
+        return creditsRequired - creditsEarned;
+    }
+
+    @Override
+    public String greeting() {
+        
+        return super.greeting() + "My mamjor is " + major + ". My GPA is " + gpa +
+        " and I have earned " + creditsEarned;
+    }
+
+    @Override
+    public Boolean isMajorComplete(Integer creditsRequired) {
+        return creditsRemaining(creditsRequired) == 0;
+
+    }
+}
